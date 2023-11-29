8709863c33750ba667d46bb1bc820ae43a379669
Baptiste Truchet <baptiste.truchet@gmail.com>
2023-11-29 14:02:04 +0100

    Add filePath as argv input

--- main.go
+++ main.go
@@ -2,15 +2,17 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/baptistetruchet/aoc2023/utils"
 	_ "github.com/samber/lo"
 )
 
 func main() {
+	filePath := os.Args[2]
 	lines := make(chan string)
 
-	go utils.ReadLines("input.txt", lines, func(line string) string { return line })
+	go utils.ReadLines(filePath, lines, func(line string) string { return line })
 
 	fmt.Println(utils.ChanToSlice(lines))
 }
