b5aa05fcc1c469afcd6701e858ae67ae39a8aac1
Baptiste Truchet <baptiste.truchet@gmail.com>
2023-12-01 13:02:12 +0100

    clean day 1

--- days/01/main.go
+++ days/01/main.go
@@ -10,32 +10,51 @@ import (
 	"github.com/baptistetruchet/advent-of-code-2023/utils"
 )
 
-func main() {
+func SolveOne() {
 	filePath := os.Args[2]
 	lines := make(chan string)
 
 	go utils.ReadLines(filePath, lines, parsing.Identity)
 
-	mapWords := map[string]int{
-		"one":   1,
-		"two":   2,
-		"three": 3,
-		"four":  4,
-		"five":  5,
-		"six":   6,
-		"seven": 7,
-		"eight": 8,
-		"nine":  9,
-		"0":     0,
-		"1":     1,
-		"2":     2,
-		"3":     3,
-		"4":     4,
-		"5":     5,
-		"6":     6,
-		"7":     7,
-		"8":     8,
-		"9":     9,
+	r := regexp.MustCompile(`\d`)
+
+	res := 0
+	for line := range lines {
+		nums := r.FindAllString(line, -1)
+		a := nums[0]
+		b := nums[len(nums)-1]
+		res += utils.ParseInt(a + b)
+	}
+
+	fmt.Println(res)
+}
+
+func SolveTwo() {
+	filePath := os.Args[2]
+	lines := make(chan string)
+
+	go utils.ReadLines(filePath, lines, parsing.Identity)
+
+	mapWords := map[string]string{
+		"one":   "1",
+		"two":   "2",
+		"three": "3",
+		"four":  "4",
+		"five":  "5",
+		"six":   "6",
+		"seven": "7",
+		"eight": "8",
+		"nine":  "9",
+		"0":     "0",
+		"1":     "1",
+		"2":     "2",
+		"3":     "3",
+		"4":     "4",
+		"5":     "5",
+		"6":     "6",
+		"7":     "7",
+		"8":     "8",
+		"9":     "9",
 	}
 	words := make([]string, 0, len(mapWords))
 	rWords := make([]string, 0, len(mapWords))
@@ -51,8 +70,7 @@ func main() {
 	for line := range lines {
 		a := mapWords[r1.FindString(line)]
 		b := mapWords[utils.ReverseString(r2.FindString(utils.ReverseString(line)))]
-
-		res += utils.ParseInt(fmt.Sprintf("%v%v", a, b))
+		res += utils.ParseInt(a + b)
 	}
 
 	fmt.Println(res)
--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"time"
 
+	day01 "github.com/baptistetruchet/advent-of-code-2023/days/01"
+)
+
+func main() {
+	start := time.Now()
+	defer func() { fmt.Println(time.Since(start)) }()
+
+	day01.SolveTwo()
 }
